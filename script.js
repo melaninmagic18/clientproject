0a883586fabfd8370e928178083cdf212d0c42f2
Jaivonah Adside <jjtheartist23@gmail.com>
2019-05-08 22:09:31 +0000

    message

---
+++ script.js
@@ -0,0 +1,27 @@
+/* global $ */
+
+$(document).ready(function() {
+    
+
+$(".click").click(function() {
+        var searchTerm =$(".enter").val();
+        var musicList = "https://api.soundcloud.com/tracks?q="+searchTerm+" &client_id=5aa8e389ba4e24b6106af5159ab3e344";
+        
+        console.log(searchTerm);
+        
+        
+        $.ajax({
+          url: musicList,
+          method: "GET",
+          success: function(response) {
+              var imageSrc= response[0].artwork_url;
+              var imageHtml= "<img src='"+imageSrc+"' >  " ;
+              
+            console.log(imageSrc);
+              $(document.body).append(imageHtml);
+          }
+        }); 
+    });
+    
+    
+});
